2e327250e64be1ab2d551ffbacd08c98175cb0a7
Jvs <jagtapvs9832@gmail.com>
2021-11-06 17:27:19 +0530

    unmount problem solved

--- src/Pages/h.js
+++ src/Pages/h.js
@@ -1,3 +1,18 @@
+import '../Css/Home.css';
+import SlideBars from './Components/SlidBars';
 export default function H() {
-    return <> <h1> hello this is home</h1> </>
+    console.log("in the Home");
+    var software = "Software";
+    var webdeveloper = "<Web Developer />";
+    return <div className="HomePage">
+        <pre id="HomeContent">
+            Hey there,<br></br>
+            I'm Jagtap Vivek..<br></br>
+            <div className="desc">
+                : Looking For The Job Opportunity <br></br>as<br></br> {software} , {webdeveloper}
+            </div>
+            <SlideBars />
+        </pre>
+
+    </div>
 }
