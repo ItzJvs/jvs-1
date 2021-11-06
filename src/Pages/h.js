ae8f980fc4b3c9f9a44e16996cf95759339baa2b
Jvs <jagtapvs9832@gmail.com>
2021-11-06 17:30:07 +0530

    unmount problem solved

--- src/Pages/h.js
+++ src/Pages/h.js
@@ -11,7 +11,7 @@ export default function H() {
             <div className="desc">
                 : Looking For The Job Opportunity <br></br>as<br></br> {software} , {webdeveloper}
             </div>
-            <SlideBars />
+            {/* <SlideBars /> */}
         </pre>
 
     </div>
